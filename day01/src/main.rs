93c8a543817536655a7a32e2e181559cfa5db673
Joel Baker <joelbaker86@gmail.com>
2022-12-02 00:50:20 -0800

    day01 **

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -16,18 +16,26 @@ fn main() {
     
     let elves: Vec<&str> = split.collect();
 
-    let mut elf_calories = Vec::<i64>::new();
+    let mut elf_calories = Vec::<u32>::new();
 
     for elf in elves {
-        // let calories: Vec<i64> = elf.lines().map(|x| x.parse::<i64>().unwrap()).collect();
-        let calories = elf.lines().map(|x| x.parse::<i64>().unwrap());
-        elf_calories.push(calories.sum::<i64>());
+        let calories = elf.lines().map(|x| x.parse::<u32>().unwrap());
+        elf_calories.push(calories.sum::<u32>());
     }
 
-    let max_value = elf_calories.iter().max();
-    match max_value {
-        Some(max) => println!( "Max value: {}", max),
-        None      => println!( "Vector is empty"),
-    }
+    // Use this for first star
+    // let max_value = elf_calories.iter().max();
+    // match max_value {
+    //     Some(max) => println!( "Max value: {}", max),
+    //     None      => println!( "Vector is empty"),
+    // }
 
+    // Use this for the second star
+    elf_calories.sort();
+    // let l = elf_calories.len();
+    // let top_three = elf_calories[l-1] + elf_calories[l-2] + elf_calories[l-3];
+    // println!("{}", top_three);
+
+    println!("{}", elf_calories.into_iter().rev().take(3).sum::<u32>());
+    
 }
