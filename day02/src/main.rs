ac733465d50e669b0a3d2d1b8329a500b7cd5d67
Joel Baker <joelbaker86@gmail.com>
2022-12-02 23:31:28 -0800

    day02 **

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,24 +1,48 @@
 use std::fs;
 
+// fn rps(line: &str) -> u32{
+//     if line == "A X" {
+//         return 1 + 3;
+//     } else if line == "B X" {
+//         return 1 + 0;
+//     } else if line == "C X" {
+//         return 1 + 6;
+//     } else if line == "A Y" {
+//         return 2 + 6;
+//     } else if line == "B Y" {
+//         return 2 + 3;
+//     } else if line == "C Y" {
+//         return 2 + 0;
+//     } else if line == "A Z" {
+//         return 3 + 0;
+//     } else if line == "B Z" {
+//         return 3 + 6;
+//     } else if line == "C Z" {
+//         return 3 + 3;
+//     } else {
+//         return 0
+//     }
+// }
+
 fn rps(line: &str) -> u32{
     if line == "A X" {
-        return 1 + 3;
+        return 3 + 0;
     } else if line == "B X" {
         return 1 + 0;
     } else if line == "C X" {
-        return 1 + 6;
+        return 2 + 0;
     } else if line == "A Y" {
-        return 2 + 6;
+        return 1 + 3;
     } else if line == "B Y" {
         return 2 + 3;
     } else if line == "C Y" {
-        return 2 + 0;
+        return 3 + 3;
     } else if line == "A Z" {
-        return 3 + 0;
+        return 2 + 6;
     } else if line == "B Z" {
         return 3 + 6;
     } else if line == "C Z" {
-        return 3 + 3;
+        return 1 + 6;
     } else {
         return 0
     }
