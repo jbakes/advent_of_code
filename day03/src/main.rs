32f2c0304e9f77134b2a85f77bd38abf896f693d
Joel Baker <joelbaker86@gmail.com>
2022-12-03 00:50:14 -0800

    day02 *

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs;
+
+fn get_common_item(s: &str) -> u32 {
+    let l = s.chars().count();
+    let half = l/2;
+    let s1 = &s[..half];
+    let s2 = &s[s.len()/2..];
+
+    for c in s1.chars() { 
+        if s2.find(c) != None {
+            return char_to_int(c);
+        }
+    }
+    return 0;
+}
+
+fn char_to_int(c: char) -> u32 {
+    if c as u32 > 90 { //lowercase
+        return c as u32 - 'a' as u32 + 1;
+    } else { //uppercase
+        return c as u32 - 'A' as u32 + 27;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("sacks.txt")
+        .expect("Should have been able to read the file");
+    
+    let priorities: _ = contents.lines().map(|x| get_common_item(x)).sum::<u32>();
+    println!("{}", priorities)
 }
